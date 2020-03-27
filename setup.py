be87433b270d2322f4c19fa55c55d7e3be6fdbfe
Viking <wiktor.przadka.1@gmail.com>
2020-03-27 16:05:23 +0100

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='cube_fractal',
+    author='Wiktor Prządka',
+    licence='MIT',
+    url='https://github.com/wprzadka/cube_fractal',
+    install_requires=[
+        "pygame==1.9.6",
+        "PyOpenGL==3.1.0",
+        "numpy==1.16.4",
+    ]
+)
